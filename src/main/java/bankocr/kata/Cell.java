85d41af5f026cdc52c991646f5601d82d5e95282
andreamjc <xandreaxbora@gmail.com>
2019-12-04 10:50:02 +0100

    Added all numbers in Cell.java

--- src/main/java/bankocr/kata/Cell.java
+++ src/main/java/bankocr/kata/Cell.java
@@ -1,11 +1,24 @@
 package bankocr.kata;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cell {
 
-    private static final String ZERO_CELL =
-                    " _ " +
-                    "| |" +
-                    "|_|";
+    private static Map<String, String> NumbToInt = new HashMap<>();
+    static {
+        NumbToInt.put(" _ " + "| |" + "|_|", "0");
+        NumbToInt.put("   " + "  |" + "  |", "1");
+        NumbToInt.put(" _ " + " _|" + "|_ ", "2");
+        NumbToInt.put(" _ " + " _|" + " _|", "3");
+        NumbToInt.put("   " + "|_|" + "  |", "4");
+        NumbToInt.put(" _ " + "|_ " + " _|", "5");
+        NumbToInt.put(" _ " + "|_ " + "|_|", "6");
+        NumbToInt.put(" _ " + "  |" + "  |", "7");
+        NumbToInt.put(" _ " + "|_|" + "|_|", "8");
+        NumbToInt.put(" _ " + "|_|" + " _|", "9");
+    }
+
     private String cellAsText;
 
     public Cell(String cellAsText) {
@@ -14,6 +27,11 @@ public class Cell {
 
     @Override
     public String toString() {
-        return ZERO_CELL.equals(cellAsText) ? "0": "1";
+        for(String key : NumbToInt.keySet()){
+            if(cellAsText.equals(key)){
+                return NumbToInt.get(key);
+            }
+        }
+        return null;
     }
 }
